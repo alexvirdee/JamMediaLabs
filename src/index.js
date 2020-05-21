aa8f968e25e9f73724035dcf6ac73fedb1aa6422
Alexinder Virdee <alex.virdee08@gmail.com>
2020-05-21 19:41:20 -0400

    Setup initial landing page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './index.scss';
 
 import MainDemo from './home/MainDemo';
+import error404 from './elements/error404';
 
 import * as serviceWorker from './serviceWorker';
 
 class Root extends Component {
   render() {
     return (
-      <h1>JAM Media Labs, LLC</h1>
+      <BrowserRouter basename={'/'}>
+          <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={MainDemo}/>
+          <Route path={`${process.env.PUBLIC_URL}/404`} component={error404}/>
+          <Route component={error404}/>
+          </Switch>
+        </BrowserRouter>
     )
   }
 }
